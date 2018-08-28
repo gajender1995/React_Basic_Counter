a76f14c7e1d10515a7e723df577b860533524ea4
gajender1995 <gajendersingh1995@gmail.com>
2018-08-28 14:56:08 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -4,37 +4,46 @@ import './App.css';
 
 
 class Mbutton extends React.Component {
-render(){
-return (
-<button onClick = { () =>  this.props.onClickArg(this.props.value)} >+{this.props.value}</button>
-);
-}
+    handelClick = () => {
+      this.props.onClickArg(this.props.value)
+    }
+    render() {
+        return ( <
+            button onClick = { this.handelClick } > +{ this.props.value } < /button>
+        );
+    }
 }
 
 const Myresult = (props) => {
-return (
-<div>{props.value}</div> 
-);
+    return ( <
+        div > { props.value } < /div> 
+    );
 }
 
 class AppMain extends React.Component {
-state = { counter : 1};
-handelEvent = (value) => {
-this.setState((prevState) => ({
-counter : this.state.counter +value 
-}));
-}
-render(){
-return (
-<div> Counter Increment <br/>
-<Mbutton value={1} onClickArg={this.handelEvent}/>
-<Mbutton value={2} onClickArg={this.handelEvent}/>
-<Mbutton value={5} onClickArg={this.handelEvent}/>
-<Mbutton value={10} onClickArg={this.handelEvent}/>
-<Myresult value={this.state.counter}/>
-</div>
-);
-}
+    state = { counter: 1 };
+    handelEvent = (value) => {
+        this.setState((prevState) => ({
+            counter: this.state.counter + value
+        }));
+    }
+    render() {
+        return ( <
+            div > Counter Increment < br / >
+            <
+            Mbutton value = { 1 } onClickArg = { this.handelEvent }
+            /> <
+            Mbutton value = { 2 } onClickArg = { this.handelEvent }
+            /> <
+            Mbutton value = { 5 } onClickArg = { this.handelEvent }
+            /> <
+            Mbutton value = { 10 } onClickArg = { this.handelEvent }
+            /> <
+            Myresult value = { this.state.counter }
+            /> <
+            /div>
+        );
+    }
 }
 
 
